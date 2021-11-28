4ed72046cd81ef84b398c008d9ed5f69880a46a1
Paulo Dutra <dutra.phs@gmail.com>
2021-11-27 22:07:29 -0300

    Add project folder

---
+++ 100DayOfCode_Bootcamp_2022/Beginner/Day2/main_day2.py
@@ -0,0 +1,33 @@
+#Data types
+
+#String
+str = "Hello"
+print(type(str))
+
+mystery = 734_529.678
+print(type(mystery))
+#Integer
+integer = 123
+print(type(integer))
+
+#Float
+flt = 3.1415
+print(type(flt))
+
+#Boolean
+boole1 = True
+boole2 = False
+print(type(boole1))
+print(type(boole2))
+
+# nome = input("seu nome? ")
+#
+# num_char = len(nome)
+#
+# print(f"seu nome tem {num_char} letras ")
+#
+
+num = input("Enter the 2 digit number: ")
+n1, n2 = int(num[0]), int(num[1])
+print(f"the sum between the first 2 digits is: {n1 + n2}")
+
