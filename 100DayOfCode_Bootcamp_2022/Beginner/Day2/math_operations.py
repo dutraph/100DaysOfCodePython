430b503adff4a39ce6d36f3c5508f1e5a4423972
Paulo Dutra <dutra.phs@gmail.com>
2021-11-27 22:28:17 -0300

    Remove

--- 100DayOfCode_Bootcamp_2022/Beginner/Day2/math_operations.py
+++
@@ -1,9 +0,0 @@
-#PEMDASLR - Priority order
-
-#Parentesis ()
-#Exponets **
-#Multiplication * #Division /
-#Addition + #Subtraction -
-#Left
-#Right
-print(3 * 3 + 3 / 3 - 3)
--- 100DayOfCode_Bootcamp_2022/Beginner/Day2/tip_calculator.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the tip calculator.")
-bill = float(input("What's the total bill? "))
-percent = int(input("What's the percentage tip would you like to give? [10, 12, or 15] "))
-people = int(input("How many people to split the bill? "))
-
-pay_per_person = (bill * (percent / 100) + bill) / people
-
-print(f"Each person should pay: ${pay_per_person:.2f}")
