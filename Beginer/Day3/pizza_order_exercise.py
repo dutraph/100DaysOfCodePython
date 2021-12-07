3c9ab36ff72ca16d9e5657f1de67e4ee5bbae12f
Paulo Dutra <dutra.phs@gmail.com>
2021-12-07 17:15:06 -0300

    add new files2

--- Beginer/Day3/pizza_order_exercise.py
+++ Beginer/Day3/pizza_order_exercise.py
@@ -1,1 +1,17 @@
-print("Welcome")
+print("Welcome to the Python Pizza Deliveries")
+
+
+def size():
+    pizza_size = input("Enter the pizza size [s / m / l]: ")
+    while True:
+        if pizza_size != "m" or pizza_size != "m" or pizza_size != 'l':
+            pizza_size = input("Enter a valid size s, m or l: ")
+        else:
+            break
+    return pizza_size
+
+
+size()
+#
+# add_peperoni
+# add_cheese
