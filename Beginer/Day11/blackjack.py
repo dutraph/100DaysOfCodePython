d1d8ba78f49585fa9aaf7d3be1cb91caa6bfe218
Paulo Dutra <dutra.phs@gmail.com>
2022-05-24 23:21:27 +0100

    Update blackjack file

--- Beginer/Day11/blackjack.py
+++ Beginer/Day11/blackjack.py
@@ -11,32 +11,52 @@ user_cards = []
 cpu_cards = []
 
 for _ in range(2):
-    new_card = deal_card()
-    user_cards.append(new_card)
+    user_cards.append(deal_card())
+    cpu_cards.append(deal_card())
 
-soma = 0
-game_over = False
-while not game_over:
-    card1 = deal_card()
-    card2 = deal_card()
-    soma = card2 + card1
-    if soma <= 21:
-        print(f'{card1}, {card2}')
-        another_card = input("Want another card (y/n)? ")
-        if another_card == 'y':
-            card3 = deal_card()
-            soma += card3
-    else:
-        game_over = True
 
-print(soma)
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
 
-# card1 = random.choice(cards)
-# card2 = random.choice(cards)
-#
-# print(f"{card1} , {card2}")
-#
-#
-# soma = card2 + card1
-# print(f"[{card1}, {card2}] = {soma}")
+
+def show_cards():
+    print(f"User:{user_cards}")
+    print(f"CPU:{cpu_cards}")
+
+
+user_score = calculate_score(user_cards)
+cpu_score = calculate_score(cpu_cards)
+
+
+while (user_score or cpu_score) <= 21:
+    print(f'Your cards{user_cards} = {user_score}')
+    print(f'CPU cards{cpu_cards} = {cpu_score}')
+    cpu_cards.append(deal_card())
+    cpu_score = calculate_score(cpu_cards)
+    new_card = input("want a new card (y/n)? ")
+    if new_card == 'y':
+        user_cards.append(deal_card())
+        user_score = calculate_score(user_cards)
+
+    if user_score == cpu_score:
+        print("Draw")
+        show_cards()
+        break
+    elif cpu_score > 21 and user_score == 21:
+        print("You Win")
+        show_cards()
+        print(f'User:{user_score} / CPU:{cpu_score}')
+        break
+    elif (user_score > 21) or (21 - user_score) > (21 - cpu_score):
+        print("CPU Wins")
+        show_cards()
+        print(f'User:{user_score} / CPU:{cpu_score}')
+        break
+
+
 
