a59fb4e90a831a286920cfc05d984643d9907156
Paulo Silva <dutra.phs@gmail.com>
2022-07-23 00:24:41 +0100

    Finish day12

---
+++ Beginer/Day12/guessing_game.py
@@ -0,0 +1,47 @@
+from random import randint
+
+EASY_LEVEL = 10
+HARD_LEVEL = 5
+
+
+def set_difficulty():
+    difficulty = int(input("Choose a difficulty: 1=Easy / 2=Hard: "))
+    if difficulty == 2:
+        return HARD_LEVEL
+    elif difficulty == 1:
+        return EASY_LEVEL
+
+
+def check_answer(number, chances):
+    while not chances == 0:
+        guess = int(input("Guess a number: "))
+        if guess > number:
+            print("Too High")
+            chances -= 1
+            print(f"You have {chances} left!")
+        elif guess < number:
+            print("Too low")
+            chances -= 1
+            print(f"You have {chances} left!")
+        elif guess == number:
+            print("You've got it")
+            break
+        elif chances == 0:
+            print("You're running out of chances")
+
+
+def game():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm Thinking a number between 1 and 100!")
+    chances = set_difficulty()
+    number = randint(0, 101)
+    print(number)
+    print(f"Your have {chances} attempts to guess the number. Good Luck!")
+    check_answer(number, chances)
+    replay = input("Play again? y/n: ")
+    if replay == 'y':
+        game()
+        set_difficulty(EASY_LEVEL)
+
+
+game()
--- Beginer/Day12/scopes.py
+++ Beginer/Day12/scopes.py
@@ -1,10 +1,19 @@
 # Global scope
 enemies = 1
 
+
 def increase_enemies():
     # Local scope
-    enemies = 2
+    # global enemies  # Used to modify the variable scope (avoid doing this)
+    # enemies = 2
     print(f'Enemies inside the function {enemies}')
+    return enemies + 1
 
-increase_enemies()
+
+enemies = increase_enemies()
 print(f'Enemies outside the function {enemies}')
+
+# Defining Global constants
+
+PI = 3.14159
+URL = 'https://www.google.com'
